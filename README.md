e5bf019f3e95aafc3a9d165cbde4682fd46c8445
Nkepers <nick.kepers.coder@gmail.com>
2021-10-24 21:11:12 -0700

    created js code for displaying the weather and added bootstraps to the html

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# weatherDashboard
+# WeatherDashboard
+
+## Description
+HTML 
+• Used to create elements for each section regarding city searches and weather displays.
+• Optimized code by using Bootstrap classes to be able to organize the content with the html file. 
+
+CSS
+• Used Bootstrap to customize buttons, dates and icons. 
+
+Javascript
+• Created an interactive weather dashboard where you can enter city names and get the current weather display plus an additional 5 day forecast.
+• UV index presented with a color that indicates whether the conditions are favorable, moderate, or severe.
+
+## Production Website
+
+[Weather Dashboard](https://nkepers.github.io/weatherDashboard/ "Weather Dashboard")
+
+## Usage
+What your site will look like on your device
+![alt text]()
+
+## Credits
+
+To give thanks to all who have helped create this product.
+
+[University of Arizona Bootcamp](https://courses.bootcampspot.com "UofA")
+
+[MDN Web DOCs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/aside "MDN")
+
+## License
+
+©Copyright 2021,  All Rights Reserved
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,71 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
 var weatherDisplay = document.getElementById('weatherDisplay');
 var futureForecast = document.getElementById('futureForecast');
 var date = document.getElementById('date1');
 var day = document.getElementById('day1');
+var search = document.getElementById('search');
+var searchBtn = document.getElementById('searchBtn');
+var citySearch = document.getElementById('citySearch');
 
 function futureWeather(forecast) {
+    futureForecast.innerHTML = "";
     var currentDay = document.createElement('div');
-    var currentDate = new Date(forecast[0].dt * 1000).totalLocaleDateString('en-US');
+    var currentDate = new Date(forecast[0].dt * 1000).toLocaleDateString('en-US');
+    console.log(currentDate)
     currentDay.textContent = currentDate;
     futureForecast.appendChild(currentDay);
+
     for (i = 0; i < 5; i++) {
 
+        var searchDate = document.getElementById(`date${i + 1}`)
+        searchDate.innerHTML = "";
+
+        var searchDay = document.getElementById(`day${i + 1}`);
+        searchDay.innerHTML = "";
+
+        var futureWeatherIconUrl = `https://openweathermap.org/img/w/${forecast[i].weather[0].icon}.png`
+        console.log(futureWeatherIconUrl);
+        var weatherIcon = document.createElement("img")
+        weatherIcon.setAttribute("src", futureWeatherIconUrl);
+        weatherIcon.classList.add("icons");
+        document.getElementById(`day${i + 1}`).appendChild(weatherIcon);
+
         var futureDay = document.createElement('div');
-        var futureDate = new Date(forecast[i + 1].dt * 1000).totalLocaleDateString('en-US');
+        var futureDate = new Date(forecast[i + 1].dt * 1000).toLocaleDateString('en-US');
         futureDay.textContent = futureDate;
         document.getElementById(`date${i + 1}`).appendChild(futureDay)
 
-        var futureTemp = data.future.temp;
-        console.log(forecast[i].temp)
+
         var temp = document.createElement('div')
-        temp.textContent = 'Temp' + ' ' + futureTemp + ' ' + '°F';
-        document.getElementById(`day${i + 1}`).appendChild(futureTemp)
+        temp.textContent = 'Temp' + ' ' + forecast[i].temp.day + ' ' + '°F';
+        document.getElementById(`day${i + 1}`).appendChild(temp)
 
-        var futureWind = data.future.wind_speed;
-        console.log(forecast[i].wind_speed)
+
         var wind = document.createElement('div')
-        wind.textContent = 'Wind Speed' + ' ' + futureWind + ' ' + 'mph';
-        document.getElementById(`day${i + 1}`).appendChild(futureWind)
+        wind.textContent = 'Wind Speed' + ' ' + forecast[i].wind_speed + ' ' + 'mph';
+        document.getElementById(`day${i + 1}`).appendChild(wind)
 
-        var futureHumidity = data.future.humidity;
-        console.log(forecast[i].humidity)
+
         var humidity = document.createElement('div')
-        humidity.textContent = 'humidity' + ' ' + futureHumidity + ' ' + '%';
-        document.getElementById(`day${i + 1}`).appendChild(futureHumidity)
+        humidity.textContent = 'humidity' + ' ' + forecast[i].humidity + ' ' + '%';
+        document.getElementById(`day${i + 1}`).appendChild(humidity)
     }
-}
+};
 
 function weatherApi(latitude, longitude) {
     var weatherApi = 'https://api.openweathermap.org/data/2.5/onecall?&lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly&appid=71938476f1f6ff4665e10e68dc4a1794&units=imperial'
-    fetch(weatherApi).then(function (respone) {
-        if (respone.ok) {
-            respone.json().then(function (data) {
+    fetch(weatherApi).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
                 console.log(data);
+
+                weatherDisplay.innerHTML = "";
+
+                var weatherIcon = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`
+                console.log(weatherIcon);
+                var iconUrl = document.createElement("img")
+                iconUrl.setAttribute("src", weatherIcon);
+                iconUrl.classList.add("icons");
+                weatherDisplay.appendChild(iconUrl)
 
                 var currentTemp = data.current.temp;
                 var tempData = document.createElement('div')
@@ -70,29 +84,34 @@ function weatherApi(latitude, longitude) {
 
                 var currentUvi = data.daily[0].uvi;
                 var uviData = document.createElement('div')
-                uviData.textContent = 'UVI' + ' ' + currentUvi + ' ';
-                // if (currentUvi < 3) {
-                //     uvi.classList.add('bg-success')
-                // }
-                // else if (currentUvi < 7) {
-                //     uvi.classList.add('bg-warning')
-                // }
-                // else {
-                //     uvi.classList.add('bg-danger')
-                // }
+                uviData.textContent = 'UVI' + ' ' + currentUvi;
+
+                if (currentUvi < 3) {
+                    uviData.classList.add('bg-success')
+                }
+                else if (currentUvi < 7) {
+                    uviData.classList.add('bg-warning')
+                }
+                else {
+                    uviData.classList.add('bg-danger')
+                }
                 weatherDisplay.appendChild(uviData)
 
+                console.log(data.daily)
                 futureWeather(data.daily);
             })
         }
     })
 };
 
-function coord(cityName) {
+function coord() {
+    let cityName = search.value
+    displaySavedStorage();
+    console.log(cityName)
     var cityLocation = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=71938476f1f6ff4665e10e68dc4a1794';
-    fetch(cityLocation).then(function (respone) {
-        if (respone.ok) {
-            respone.json().then(function (data) {
+    fetch(cityLocation).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
                 console.log(data)
                 var lat = data.coord.lat;
                 var lon = data.coord.lon;
@@ -102,4 +121,41 @@ function coord(cityName) {
     })
 };
 
-coord();
+var displayCity = function (searchTerm) {
+    var saveButton = document.createElement("button");
+    saveButton.textContent = searchTerm;
+    saveButton.classList.add("row");
+    saveButton.addEventListener("click", function () {
+        console.log("this is some text", this);
+        citySearch.textContent = this.textContent;
+        coord(this.textContent);
+        console.log(this.textContent);
+    });
+    saveCity.append(saveButton);
+};
+
+var saveStorage = function () {
+    var priorCities = JSON.parse(localStorage.getItem("location")) || [];
+    priorCities.push(search.value);
+    localStorage.setItem("location", JSON.stringify(priorCities));
+    console.log("this", priorCities);
+};
+
+var displaySavedStorage = function () {
+    var priorCities = JSON.parse(localStorage.getItem("location")) || [];
+    for (var i = 0; i < priorCities.length; i++) {
+        var priorCitySearch = document.createElement("button");
+        priorCitySearch.textContent = priorCities[i];
+        priorCitySearch.classList.add("row");
+        priorCitySearch.addEventListener("click", function () {
+        console.log("this is some text", this);
+        citySearch.textContent = this.textContent;
+        coord(this.textContent);
+        });
+        saveCity.append(priorCitySearch);
+    }
+
+};
+
+saveStorage();
+searchBtn.addEventListener('click', coord)
